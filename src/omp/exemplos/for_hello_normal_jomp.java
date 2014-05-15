d2ea2a172db7d8e20dfde590971206780c5c3fdd
pmbweber <pmbweber@MC-3295.furb.br>
2014-05-14 21:24:08 -0300

    Inittial Commit

---
+++ src/omp/exemplos/for_hello_normal_jomp.java
@@ -0,0 +1,129 @@
+package omp.exemplos;
+
+import jomp.runtime.OMP;
+
+public class for_hello_normal_jomp {
+
+
+	public static void main(String[] args) {
+		
+		int n = 3;
+		int i, total = 0;
+		int[][] matriz = new int[][]{{1, 2 ,3}, {4, 5, 6}, {7, 8, 9}};
+		
+		OMP.setNumThreads(2);
+
+// OMP PARALLEL BLOCK BEGINS
+{
+  __omp_Class0 __omp_Object0 = new __omp_Class0();
+  // shared variables
+  __omp_Object0.matriz = matriz;
+  __omp_Object0.total = total;
+  __omp_Object0.n = n;
+  __omp_Object0.args = args;
+  // firstprivate variables
+  try {
+    jomp.runtime.OMP.doParallel(__omp_Object0);
+  } catch(Throwable __omp_exception) {
+    System.err.println("OMP Warning: Illegal thread exception ignored!");
+    System.err.println(__omp_exception);
+  }
+  // reduction variables
+  // shared variables
+  matriz = __omp_Object0.matriz;
+  total = __omp_Object0.total;
+  i = __omp_Object0.i;
+  n = __omp_Object0.n;
+  args = __omp_Object0.args;
+}
+// OMP PARALLEL BLOCK ENDS
+
+		System.out.println("Total: " + total);
+	}
+
+// OMP PARALLEL REGION INNER CLASS DEFINITION BEGINS
+private static class __omp_Class0 extends jomp.runtime.BusyTask {
+  // shared variables
+  int [ ] [ ] matriz;
+  int total;
+  int i;
+  int n;
+  String [ ] args;
+  // firstprivate variables
+  // variables to hold results of reduction
+
+  public void go(int __omp_me) throws Throwable {
+  // firstprivate variables + init
+  // private variables
+  // reduction variables, init to default
+    // OMP USER CODE BEGINS
+
+		{
+                         { // OMP FOR BLOCK BEGINS
+                         // copy of firstprivate variables, initialized
+                         // copy of lastprivate variables
+                         // variables to hold result of reduction
+                         boolean amLast=false;
+                         {
+                           // firstprivate variables + init
+                           // [last]private variables
+                           // reduction variables + init to default
+                           // -------------------------------------
+                           jomp.runtime.LoopData __omp_WholeData2 = new jomp.runtime.LoopData();
+                           jomp.runtime.LoopData __omp_ChunkData1 = new jomp.runtime.LoopData();
+                           __omp_WholeData2.start = (long)( 0);
+                           __omp_WholeData2.stop = (long)( n);
+                           __omp_WholeData2.step = (long)(1);
+                           jomp.runtime.OMP.setChunkStatic(__omp_WholeData2);
+                           while(!__omp_ChunkData1.isLast && jomp.runtime.OMP.getLoopStatic(__omp_me, __omp_WholeData2, __omp_ChunkData1)) {
+                           for(;;) {
+                             if(__omp_WholeData2.step > 0) {
+                                if(__omp_ChunkData1.stop > __omp_WholeData2.stop) __omp_ChunkData1.stop = __omp_WholeData2.stop;
+                                if(__omp_ChunkData1.start >= __omp_WholeData2.stop) break;
+                             } else {
+                                if(__omp_ChunkData1.stop < __omp_WholeData2.stop) __omp_ChunkData1.stop = __omp_WholeData2.stop;
+                                if(__omp_ChunkData1.start > __omp_WholeData2.stop) break;
+                             }
+                             for(int i = (int)__omp_ChunkData1.start; i < __omp_ChunkData1.stop; i += __omp_ChunkData1.step) {
+                               // OMP USER CODE BEGINS
+{
+				for(int j = 0; j < n; j++){
+					System.out.println("Thread: " + OMP.getThreadNum() + "\u00cdndice: " + i + ", " + j);
+					total += matriz[i][j];
+				}
+			}
+                               // OMP USER CODE ENDS
+                               if (i == (__omp_WholeData2.stop-1)) amLast = true;
+                             } // of for 
+                             if(__omp_ChunkData1.startStep == 0)
+                               break;
+                             __omp_ChunkData1.start += __omp_ChunkData1.startStep;
+                             __omp_ChunkData1.stop += __omp_ChunkData1.startStep;
+                           } // of for(;;)
+                           } // of while
+                           // call reducer
+                           jomp.runtime.OMP.doBarrier(__omp_me);
+                           // copy lastprivate variables out
+                           if (amLast) {
+                           }
+                         }
+                         // set global from lastprivate variables
+                         if (amLast) {
+                         }
+                         // set global from reduction variables
+                         if (jomp.runtime.OMP.getThreadNum(__omp_me) == 0) {
+                         }
+                         } // OMP FOR BLOCK ENDS
+
+		}
+    // OMP USER CODE ENDS
+  // call reducer
+  // output to _rd_ copy
+  if (jomp.runtime.OMP.getThreadNum(__omp_me) == 0) {
+  }
+  }
+}
+// OMP PARALLEL REGION INNER CLASS DEFINITION ENDS
+
+}
+
